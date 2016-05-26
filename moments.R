f4ca1327c398297b0e3c519746ba1ffbe0c8a0ef
Jonathan Dushoff <dushoff@mcmaster.ca>
2016-05-26 18:35:42 -0400

    Some experiments in moment parameterization.

--- moments.R
+++ moments.R
@@ -1,19 +1,22 @@
 n <- 10000
 
 p <- (2*(1:n)-1)/(2*n)
-q <- qlnorm(p)
 q <- qgamma(p, shape=0.5)
+q <- qlnorm(p)
 
-wt <- exp(-1*q)
+wt <- exp(-0*q)
 T <- sum(wt)
 
-M1 <- mean(q*wt)/T
-M2 <- mean(q^2*wt)/T
-M3 <- mean(q^3*wt)/T
+M1 <- sum(q*wt)/T
+M2 <- sum(q^2*wt)/T
+M3 <- sum(q^3*wt)/T
 
 phi2 <- M2/M1^2-1
 phi3 <- M3/(M1*M2)-1
 
 rho <- phi3/phi2
 
-print(rho)
+kap3 <- M1*M3/M2^2-1
+krho <- kap3/phi2
+
+print(c(rho, krho, kap3))
