515875d8d4ac0666224e8d0bc4f0eb8aff498d31
Jonathan Dushoff <dushoff@mcmaster.ca>
2016-12-29 23:52:11 -0500

    Autocommit (notebook) On branch gh-pages Your branch is up-to-date with 'origin/gh-pages'. Changes to be committed:   (use "git reset HEAD <file>..." to unstage)
    	modified:   average.pl
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
    	banner.jpg
    	commit.time

--- average.pl
+++ average.pl
@@ -8,7 +8,12 @@ sub tree{
 	my @low = @{shift @_};
 	my @high = @{shift @_};
 	my @mid = ($low[0]+$high[0], $low[1]+$high[1]);
-	mid[2] = 0;
+	$mid[2] = int($base*$mid[0]/$mid[1] + 1/2);
+	$first[$mid[2]] = $mid[0]+$mid[1] unless defined $first[$mid[2]];
+	tree(\@low, \@mid) unless $mid[2]==$low[2];
+	tree(\@mid, \@high) unless $mid[2]==$high[2];
 }
 
-say join ", ", @{tree([0, 1], [4, 6])}; 
+tree([0, 1, 0], [1, 1, $base]); 
+
+say join " ", @first;
