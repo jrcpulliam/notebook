66088ec603b56fdbc95343a74207377bfc4de245
Jonathan Dushoff <dushoff@mcmaster.ca>
2019-03-14 22:33:36 -0400

    Final picture

--- simpson.R
+++ simpson.R
@@ -33,6 +33,18 @@ appRange <- function(com, size, reps){
 com1 <- c(1, 1, 1, 1)
 com2 <- c(1, rep(1/(mini-1), mini))
 
-print(mean(appRange(com1, 10, 1000)))
-print(mean(appRange(com2, 10, 1000)))
+r1 <- appRange(com1, 40, 1000)
 
+r2 <- appRange(com2, 40, 1000)
+
+print(mean(r1))
+print(mean(r2))
+
+print(mean(1/r1))
+print(mean(1/r2))
+
+print(hist(1-1/r1, main="Even", xlab="index"))
+print(hist(1-1/r2, main="Uneven", xlab="index"))
+
+print(hist(r1, main="Even", xlab="diversity"))
+print(hist(r2, main="Uneven", xlab="diversity"))
