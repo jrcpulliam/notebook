686315eddc6acb9d85319d26af914b2b3fd72ce7
Jonathan Dushoff <dushoff@mcmaster.ca>
2019-05-18 11:53:05 -0400

    Clean up a bit

--- maya.R
+++ maya.R
@@ -1,6 +1,9 @@
 library(googlesheets)
 library(dplyr)
 
-url <-  "https://docs.google.com/spreadsheets/d/1qTYPV7PXb_5EVyG2SoxuAdlzXRfvJxgRUOwAgbYwS7s/edit?usp=sharing"
+## If you put a key on github, you lose crypto
+## If you use these tools at all, do you necessarily lose crypto?
+
+url <-  "https://docs.google.com/spreadsheets/d/KEY/edit?usp=sharing"
 key <- extract_key_from_url(url)
 sheet <- gs_key(key)
