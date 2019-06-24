489fdd79d860438ca2e66c16b5c6a7811e006e5d
Jonathan Dushoff <dushoff@mcmaster.ca>
2019-06-24 15:21:04 -0400

    commit before refactor milli

--- milli.R
+++ milli.R
@@ -2,6 +2,7 @@ library(ggplot2)
 theme_set(theme_bw())
 library(tidyverse)
 
+
 milli <- function(tf){
 	tf <- (tf
 		%>% arrange(est)
@@ -16,7 +17,31 @@ milli <- function(tf){
 	)
 }
 
-print(milli(normT))
-print(milli(expT))
-print(milli(normX))
+slug <- function(tf){
+	numEst <- nrow(tf)
+	tf <- (tf
+		%>% arrange(est)
+		%>% mutate(
+			estQ=((1:numEst)-1/2)/numEst
+			, pos = estQ*(lower-median(lower))
+				+(1-estQ)*(upper-median(upper))
+		)
+		%>% arrange(pos)
+		%>% mutate(
+			quantile=((1:numEst)-1/2)/numEst
+		)
+	)
+
+	return(ggplot(tf, aes(x=quantile, y=est, ymin = lower, ymax=upper))
+		+ geom_pointrange(alpha=0.2, fatten=0.1)
+		+ geom_hline(yintercept=0)
+		+ xlab("index")
+		+ ylab("estimate")
+	)
+}
+
+
+print(slug(normT))
+print(slug(expT))
+print(slug(normX))
 
